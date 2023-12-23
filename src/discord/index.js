3cc2a5e3d947d721be27c86ad36184cf91a2f4b9
timeapis <153199278+timeapis@users.noreply.github.com>
2023-12-23 18:23:39 +0200

    Create index.js for discord bot

---
+++ src/discord/index.js
@@ -0,0 +1,34 @@
+const { Client, Intents } = require("discord.js");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const startBot = async () => {
+    const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+    client.once("ready", () => {
+     console.log('\x1b[33m%s\x1b[0m',"Bot is up");
+
+        const commands = client.application.commands;
+
+        fs.readdirSync("./src/discord/commands").forEach((fileName) => {
+            const command = require(`./commands/${fileName}`);
+            commands.create(command.commandInfo);
+        });
+    });
+
+    client.on("interactionCreate", (interaction) => {
+        if (!interaction.isApplicationCommand()) return;
+
+        const commandPath = `./src/discord/commands/${interaction.commandName}.js`;
+        if (fs.existsSync(commandPath)) {
+            require(commandPath).execute(interaction);
+        }
+    });
+
+    await client.login(process.env.DISCORD_BOT_TOKEN);
+};
+
+startBot();
