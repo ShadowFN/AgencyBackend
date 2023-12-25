2792179270226a1179b09826fae3f88155769c4c
timeapis <153199278+timeapis@users.noreply.github.com>
2023-12-25 21:40:46 +0200

    Update functions.js

--- src/structs/functions.js
+++ src/structs/functions.js
@@ -150,10 +150,10 @@ function getContentPages(req) {
 function getItemShop()
 {
     const catalog = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "..", "responses", "itemshop.json")).toString()
+        fs.readFileSync(path.join(__dirname, "..", "responses", "itemshop2.json")).toString() //idk why if i didnt make itemshopv2 half of the item shop wont work
     );
     const CatalogConfig = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "..", "Config", "catalog_config.json").toString())
+        fs.readFileSync(path.join(__dirname, "..", "Config", "itemshop.json").toString())
     );
 
     try
