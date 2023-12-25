93e09fffdd5daf99a7cb4b9f1224af448f807b13
timeapis <153199278+timeapis@users.noreply.github.com>
2023-12-26 00:03:33 +0200

    Update version.js

--- src/routes/version.js
+++ src/routes/version.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express.Router();
 
-const { verifyToken, verifyClient } = require("../tokenManager/tokenVerify.js");
+const { verifyToken, verifyClient } = require("../token/tokenCreation.js");
 
 app.get("/fortnite/api/version", (req, res) => {
     res.json({
