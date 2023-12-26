aa5ce88557c6733700684fb3f0e13b2dd3f7a17b
timeapis <153199278+timeapis@users.noreply.github.com>
2023-12-26 15:00:51 +0200

    Update ban.js

--- src/discord/commands/ban.js
+++ src/discord/commands/ban.js
@@ -1,5 +1,5 @@
-const User = require("../../src/model/user.js");
-const functions = require("../../src/structs/functions.js");
+const User = require("../../model/user.js");
+const functions = require("../../structs/functions.js");
 const fs = require("fs");
 const dotenv = require("dotenv");
 
