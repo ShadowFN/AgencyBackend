a25e5418505e9dd8371e954cf542eac7e8cee37d
timeapis <153199278+timeapis@users.noreply.github.com>
2023-12-26 15:28:38 +0200

    Update index.js

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,34 +1,30 @@
+require('dotenv').config();
+
 const { Client, Intents } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const fs = require("fs");
-const dotenv = require("dotenv");
 
+client.once("ready", () => {
+    console.log('\x1b[33m%s\x1b[0m',"Bot is up");
 
-dotenv.config();
+    let commands = client.application.commands;
 
-const startBot = async () => {
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+    fs.readdirSync("./src/discord/commands").forEach(fileName => {
+        const command = require(`./commands/${fileName}`);
 
-    client.once("ready", () => {
-     console.log('\x1b[33m%s\x1b[0m',"Bot is up");
-
-        const commands = client.application.commands;
-
-        fs.readdirSync("./discord/commands").forEach((fileName) => {
-            const command = require(`./commands/${fileName}`);
-            commands.create(command.commandInfo);
-        });
+        commands.create(command.commandInfo);
     });
+});
 
-    client.on("interactionCreate", (interaction) => {
-        if (!interaction.isApplicationCommand()) return;
+client.on("interactionCreate", interaction => {
+    if (!interaction.isApplicationCommand()) return;
 
-        const commandPath = `./discord/commands/${interaction.commandName}.js`;
-        if (fs.existsSync(commandPath)) {
-            require(commandPath).execute(interaction);
-        }
-    });
+    if (fs.existsSync(`./src/discord/commands/${interaction.commandName}.js`)) {
+        require(`./commands/${interaction.commandName}.js`).execute(interaction);
+    }
+});
 
-    await client.login(process.env.DISCORD_BOT_TOKEN);
-};
+// Access environment variables
+const discordBotToken = process.env.DISCORD_BOT_TOKEN;
 
-startBot();
+client.login(discordBotToken);
