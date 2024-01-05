2ddf9d76c3bb210365c47d28729d1172ea2e87d7
Marvelco <153199278+MarvelcoOGFN@users.noreply.github.com>
2024-01-05 23:42:03 +0200

    Update profile.js

--- src/structs/profile.js
+++ src/structs/profile.js
@@ -1,61 +1,36 @@
-const fs = require('fs').promises;
+const fs = require("fs");
 
-async function createProfiles(accountId) {
-    const profiles = {};
+function createProfiles(accountId) {
+    let profiles = {};
 
-    try {
-        const files = await fs.readdir('./src/Profiles');
+    fs.readdirSync("./Profiles").forEach(fileName => {
+        const profile = require(`../src/Profiles/${fileName}`);
 
-        for (const fileName of files) {
-            const filePath = `../Profiles/${fileName}`;
-            const profile = require(filePath);
+        profile.accountId = accountId;
+        profile.created = new Date().toISOString();
+        profile.updated = new Date().toISOString();
 
-            profile.accountId = accountId;
-            profile.created = new Date().toISOString();
-            profile.updated = new Date().toISOString();
-
-            profiles[profile.profileId] = profile;
-        }
-    } catch (error) {
-        console.error('Error reading default profiles:', error.message);
-    }
+        profiles[profile.profileId] = profile;
+    });
 
     return profiles;
 }
 
 async function validateProfile(profileId, profiles) {
     try {
-        const profile = profiles[profileId];
+        let profile = profiles.profiles[profileId];
 
         if (!profile || !profileId) {
-            throw new Error('Invalid profile/profileId');
+            throw new Error("Invalid profile/profileId");
         }
-    } catch (error) {
-        console.error('Error validating profile:', error.message);
+    } catch {
         return false;
     }
 
     return true;
 }
 
-async function updateProfile(profileId, profiles, updatedData) {
-    try {
-        const profile = profiles[profileId];
-
-        if (!profile) {
-            throw new Error('Profile not found');
-        }
-
-        Object.assign(profile, updatedData);
-        profile.updated = new Date().toISOString();
-    } catch (error) {
-        console.error('Error updating profile:', error.message);
-        throw error;
-    }
-}
-
 module.exports = {
     createProfiles,
-    validateProfile,
-    updateProfile,
+    validateProfile
 };
